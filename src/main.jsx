7ed378de6ef0ef3ceaad2c393a14c0102973a243
A7med <medotamer861@gmail.com>
2026-09-08 20:17:07 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 
@@ -13,3 +14,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+
+
