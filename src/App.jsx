b184a42eaf8264055e858f4e7273d8dacb0fb566
Ahmed <medotamer861@gmail.com>
2026-09-08 22:24:52 +0300

    edit light-mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useEffect, useState } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
@@ -8,20 +8,64 @@ import JobDetails from "./pages/JobDetails";
 import Apply from "./pages/Apply";
 import SavedJobs from "./pages/SavedJobs";
 import NotFound from "./pages/NotFound";
-import "./App.css"
+
+import "./App.css";
 
 function App() {
+  // Load saved theme
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("hireflow-theme") === "dark";
+  });
+
+  // Apply theme
+  useEffect(() => {
+    if (darkMode) {
+       document.body.classList.remove("light-mode");
+      document.body.classList.add("dark-mode");
+      localStorage.setItem("hireflow-theme", "dark");
+    } else {
+      document.body.classList.remove("dark-mode");
+       document.body.classList.add("light-mode");
+      localStorage.setItem("hireflow-theme", "light");
+    }
+  }, [darkMode]);
+
+  // Toggle theme
+  function toggleTheme() {
+    setDarkMode((prev) => !prev);
+  }
+
   return (
     <HashRouter>
-      <Navbar />
-      
+      <Navbar
+        darkMode={darkMode}
+        toggleTheme={toggleTheme}
+      />
+
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/jobs" element={<Jobs />} />
-        <Route path="/jobs/:id" element={<JobDetails />} />
-        <Route path="/jobs/:id/apply" element={<Apply />} />
-        <Route path="/saved-jobs" element={<SavedJobs />} />
-        <Route path="*" element={<NotFound />} />
+
+        <Route
+          path="/jobs/:id"
+          element={<JobDetails />}
+        />
+
+        <Route
+          path="/jobs/:id/apply"
+          element={<Apply />}
+        />
+
+        <Route
+          path="/saved-jobs"
+          element={<SavedJobs />}
+        />
+
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
       </Routes>
     </HashRouter>
   );
